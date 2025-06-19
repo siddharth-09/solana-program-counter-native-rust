7e17e935de97914c9f31eafd24e417221decb72b
Siddharth Panchal <1234siddharth.panchal@gmail.com>
2025-06-19 18:14:55 +0530

    Rust Native Solana Contract

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+account_info::{next_account_info, AccountInfo}, entrypoint::{ ProgramResult}, entrypoint, msg, pubkey::Pubkey
+};
+
+#[derive(BorshSerialize,BorshDeserialize)]
+enum InstructionType{
+    Increment(u32),
+    Decrement(u32)
+}
+#[derive(BorshSerialize,BorshDeserialize)]
+struct Counter{
+    count:u32
+}
+
+entrypoint!(counter_contract);
+
+pub fn counter_contract(
+    program_id:&Pubkey,
+    accounts:&[AccountInfo],//Adress of array 
+    instruction_data : &[u8] // bytes of data can come from user represent the request they want to do
+)->ProgramResult{
+    
+    let acc = next_account_info(&mut accounts.iter())?;
+    let mut counter  = Counter::try_from_slice(&acc.data.borrow())?;
+
+    match InstructionType::try_from_slice(instruction_data)? {
+
+        InstructionType::Increment(value)=>{
+            msg!("Increment Succedd");
+            counter.count += value;
+        },
+
+        InstructionType::Decrement(value)=>{
+            msg!("Decrement Succedd");
+            counter.count -= value;
+        }
+
+    }
+
+    counter.serialize(&mut *acc.data.borrow_mut())?;
+    
+    msg!("Contract Succedd");
+    Ok(())
+}
